915e516a40ef15e6614eb5c37e1acf1a92a3429c
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-10 10:31:58 -0300

    requisito 5 e 6 completo

--- script.js
+++ script.js
@@ -18,7 +18,21 @@ const criarInput = document.createElement('input');
 pegarBody.appendChild(criarInput);
 criarInput.id = 'texto-tarefa';
 
+// Requisito 5
+const criarBotao = document.createElement('button');
+pegarBody.appendChild(criarBotao);
+criarBotao.id = 'criar-tarefa'
+criarBotao.innerHTML = 'Criar Nova Tarefa'
+
 // Requisito 4
 const criarOl = document.createElement('ol');
 pegarBody.appendChild(criarOl);
 criarOl.id = 'lista-tarefas';
+
+// Requisito 6
+criarBotao.addEventListener('click', function(){
+  const criarLi = document.createElement('li');
+  criarOl.appendChild(criarLi);
+  criarLi.innerHTML = criarInput.value;
+  criarInput.value = '';
+});
