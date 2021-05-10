02bc427830bf3af5d8399fd4c1c33e960c1cec90
Lucas de Almeida Santos <lucasquearis@hotmail.com>
2021-05-10 09:51:47 -0300

    requisito 2 completo

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
+//Requisito 1
 const criarHeader = document.createElement('header');
-const pegarBody = document.body
-const criarH1 = document.createElement('h1')
+const pegarBody = document.body;
+const criarH1 = document.createElement('h1');
 
 pegarBody.appendChild(criarHeader);
 criarHeader.appendChild(criarH1);
-criarH1.innerHTML = 'Minha Lista de Tarefas'
+criarH1.innerHTML = 'Minha Lista de Tarefas';
+
+//Requisito 2
+const criarParagrafo = document.createElement('p');
+pegarBody.appendChild(criarParagrafo);
+criarParagrafo.id = 'funcionamento'
+criarParagrafo.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
